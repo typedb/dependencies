ee376afe7ffcab2e0c69342e1442ff43a75e352d
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-05 23:24:52 +0000

    update test-coverage

--- ci/run-bazel.sh
+++ ci/run-bazel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo $BUILD_WORKSPACE_DIRECTORY
 if [[ -f ~/.config/gcloud/application_default_credentials.json ]]; then
     echo "Bazel will be executed with RBE support. This means the build is remotely executed and the cache will be re-used by subsequent CI jobs."
     CMD="$@ --config=rbe"
