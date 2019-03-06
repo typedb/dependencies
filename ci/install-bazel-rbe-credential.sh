6ff75c335b61d7a00f5b03fad177dcfd567d7399
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-06 12:14:13 +0000

    removed install-bazel and add install-bazel-rbe-credential

--- ci/install-bazel-rbe-credential.sh
+++ ci/install-bazel-rbe-credential.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-############################
-#      install bazel       #
-############################
-
-echo "Installing Bazel..."
-curl -OL https://github.com/bazelbuild/bazel/releases/download/0.20.0/bazel-0.20.0-installer-linux-x86_64.sh
-chmod +x bazel-0.20.0-installer-linux-x86_64.sh
-sudo ./bazel-0.20.0-installer-linux-x86_64.sh
-echo "Bazel has been installed!"
-
-
-############################
-#  install RBE credential  #
-############################
-
 echo "Installing RBE credential..."
 if [[ -n "$BAZEL_RBE_CREDENTIAL" ]]; then
     BAZEL_RBE_CREDENTIAL_LOCATION=~/.config/gcloud/application_default_credentials.json
