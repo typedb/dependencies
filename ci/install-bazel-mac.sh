9e8fa13bb2be55743ff3775757d339769c593fb6
haikalpribadi <haikal@grakn.ai>
2019-03-19 19:33:11 +0000

    Added install-bazel-mac.sh

---
+++ ci/install-bazel-mac.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -OL https://github.com/bazelbuild/bazel/releases/download/0.20.0/bazel-0.20.0-installer-darwin-x86_64.sh
+chmod +x bazel-0.20.0-installer-darwin-x86_64.sh
+sudo ./bazel-0.20.0-installer-darwin-x86_64.sh
+rm ./bazel-0.20.0-installer-darwin-x86_64.sh
