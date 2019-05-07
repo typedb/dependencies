71ce53cf0a83fb4e6469ca9cd7389130121135eb
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-05-07 19:04:56 +0100

    fix

--- common.py
+++ common.py
@@ -1,20 +1,32 @@
 import os
 import subprocess as sp
+import tempfile
 
 
-def shell_execute(*args):
-    p = sp.Popen(args, stdout=sp.PIPE, stderr=sp.PIPE)
-    p.wait()
-    out = p.stdout.read()
-    err = p.stderr.read()
-    if p.returncode == 0:
+def shell_execute(args, **kwargs):
+    with tempfile.NamedTemporaryFile(delete = True) as tmpstdout, \
+            tempfile.NamedTemporaryFile(delete = True) as tmpstderr:
+
+        process = sp.Popen(args, stdout=tmpstdout, stderr=tmpstderr, **kwargs)
+        process.wait()
+
+        tmpstdout.seek(0)
+        out = tmpstdout.read()
         if type(out) == bytes:
             out = out.decode()
+
+        tmpstderr.seek(0)
+        err = tmpstderr.read()
         if type(err) == bytes:
             err = err.decode()
-        return out, err
-    else:
-        raise RuntimeError('An error occurred when running "' + str(' '.join(args)) + '"\n' +
-                           'Exit code = "' + str(p.returncode) + '"\n' +
-                           'stdout = "' + out + '"\n' +
-                           'stderr = "' + err + '"\n')
+
+        if process.returncode == 0:
+            print('cmd = {}\nout = {}\nerr = {}'.format(args, out, err))
+            return out, err
+        else:
+            raise RuntimeError('An error occurred.\n' +
+                               '- Command = "'+ str(args) + '"\n' +
+                               '- Exit code = "' + str(process.returncode) + '"\n' +
+                               '- Working directory = "' + str(cwd) + '"\n' +
+                               '- stdout = "' + out + '"\n' +
+                               '- stderr = "' + err + '"\n')
