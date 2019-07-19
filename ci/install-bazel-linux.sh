f960fac2f991ed7bc80715e3683df30555f65c07
Max Vorobev <vmax0770@gmail.com>
2019-07-19 13:33:02 +0300

    Upgrade bazel to 0.26.1

--- ci/install-bazel-linux.sh
+++ ci/install-bazel-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-curl -OL https://github.com/bazelbuild/bazel/releases/download/0.25.2/bazel-0.25.2-installer-linux-x86_64.sh
-chmod +x bazel-0.25.2-installer-linux-x86_64.sh
-sudo ./bazel-0.25.2-installer-linux-x86_64.sh
-rm ./bazel-0.25.2-installer-linux-x86_64.sh
+curl -OL https://github.com/bazelbuild/bazel/releases/download/0.26.1/bazel-0.26.1-installer-linux-x86_64.sh
+chmod +x bazel-0.26.1-installer-linux-x86_64.sh
+sudo ./bazel-0.26.1-installer-linux-x86_64.sh
+rm ./bazel-0.26.1-installer-linux-x86_64.sh
--- ci/install-bazel-mac.sh
+++ ci/install-bazel-mac.sh
@@ -2,7 +2,7 @@
 
 set -e
 brew cask install homebrew/cask-versions/adoptopenjdk8
-curl -OL https://github.com/bazelbuild/bazel/releases/download/0.25.2/bazel-0.25.2-installer-darwin-x86_64.sh
-chmod +x bazel-0.25.2-installer-darwin-x86_64.sh
-sudo ./bazel-0.25.2-installer-darwin-x86_64.sh
-rm ./bazel-0.25.2-installer-darwin-x86_64.sh
+curl -OL https://github.com/bazelbuild/bazel/releases/download/0.26.1/bazel-0.26.1-installer-darwin-x86_64.sh
+chmod +x bazel-0.26.1-installer-darwin-x86_64.sh
+sudo ./bazel-0.26.1-installer-darwin-x86_64.sh
+rm ./bazel-0.26.1-installer-darwin-x86_64.sh
