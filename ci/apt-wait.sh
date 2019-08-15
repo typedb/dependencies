f8415a819634412d496947780e882ab9f2b399db
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-08-15 14:36:34 +0100

    add apt-wait

--- ci/apt-wait.sh
+++ ci/apt-wait.sh
@@ -4,7 +4,7 @@ echo -n 'Waiting for the initial apt-get process to finish...'
 init_wait=0
 while [[ $init_wait -eq 0 ]]; do
   set +e
-  ps -C apt-get
+  ps -C apt-get > /dev/null
   init_wait=$?
   set -e
   echo -n '.'
