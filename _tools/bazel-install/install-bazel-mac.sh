3368ea0018b6ca1cb504c9557ea55cc0c8981f21
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-06-18 14:45:41 +0100

    Fixed more package
          - bazel build //_images/... worked
          - bazel build //_tools/checkstyle/... worked
          - bazel build //_tools/bazel-install/... worked
          - bazel build //_tools/bazel_run/... worked

---
+++ _tools/bazel-install/install-bazel-mac.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+BAZEL_VERSION=3.0.0
+
+brew cask install homebrew/cask-versions/adoptopenjdk8
+curl -OL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
+chmod +x bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
+sudo ./bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
+rm ./bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
