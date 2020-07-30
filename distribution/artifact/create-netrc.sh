63f5d73e9aeff185b1cb18a31e40ca8c8618561e
Adam Mitchell <adam@grakn.ai>
2020-07-30 17:49:08 +0100

    Update to use with 2.0 and private artifact

---
+++ distribution/artifact/create-netrc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+MISSING_VARIABLE=false
+
+if [ -z $ARTIFACT_USERNAME ]; then echo "ARTIFACT_USERNAME is not set."; MISSING_VARIABLE=true; fi;
+if [ -z $ARTIFACT_PASSWORD ]; then echo "ARTIFACT_PASSWORD is not set."; MISSING_VARIABLE=true; fi;
+
+if [ $MISSING_VARIABLE = true ]; then exit 1; fi;
+
+cat >> ${HOME}/.netrc <<EOF
+machine repo.grakn.ai
+        login $ARTIFACT_USERNAME
+        password $ARTIFACT_PASSWORD
+EOF
