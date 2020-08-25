e95e6f7b68eaa2e4908624a8cd99219a423cdacf
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-08-25 10:53:31 +0100

    Restrict the input for the tagged_deps field of release_validate_deps (#185)
    The tagged_deps field has been made to strictly accept input in the following format: `@graknlabs_common`, and not `graknlabs_common`

--- tool/release/ValidateDeps.kt
+++ tool/release/ValidateDeps.kt
@@ -8,14 +8,22 @@ fun main() {
     val workspaceRefsPath = "{workspace_refs_json_path}"
     val taggedDeps = "{tagged_deps}"
 
+    val taggedDepsSet = taggedDeps.split(",").toSet()
+    val malformedDepsSet = taggedDepsSet.filter { dep -> !dep.startsWith("@") }
+    if (malformedDepsSet.isNotEmpty()) {
+        System.err.println("\nThe following dependencies do not start with an '@': $malformedDepsSet")
+        System.err.println("Please prefix them with '@', ie., ${malformedDepsSet.map { dep -> "@" + dep }}'")
+        System.exit(1)
+    }
+
     val refs = Paths.get(workspaceRefsPath)
     val refFileReader = FileReader(refs.toFile())
     val parsed = Json.parse(refFileReader).asObject()
 
-    val taggedDepsSet = taggedDeps.split(",").toSet().map { w -> w.replace("@", "") }
-
-    val tagDepsFromRefs = parsed["tags"].asObject().names().toSet()
-    val snapshotDependencies = taggedDepsSet.subtract(tagDepsFromRefs)
+    val taggedDepsFromRefs = parsed["tags"].asObject().names().toSet()
+    val snapshotDependencies = taggedDepsSet
+            .map { w -> w.replace("@", "") }
+            .subtract(taggedDepsFromRefs)
 
     if (snapshotDependencies.size > 0) {
         System.err.println("\nThese dependencies are expected to be declared by tag instead of commit: $snapshotDependencies")
