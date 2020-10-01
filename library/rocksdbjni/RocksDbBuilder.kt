2639483bd94366ce22c4bd5e13f6f5d6f4b4b9d9
Alex Walker <alex@grakn.ai>
2020-10-01 11:47:24 +0100

    Add assemble_maven, deploy_maven, VERSION

--- library/rocksdbjni/RocksDbBuilder.kt
+++ library/rocksdbjni/RocksDbBuilder.kt
@@ -5,16 +5,16 @@ import org.zeroturnaround.exec.ProcessResult
 import java.io.File
 import java.nio.file.Paths
 
-const val VERSION = "6.8.1"
-
 fun main() {
+    val version = Paths.get("VERSION").toFile().useLines { it.firstOrNull() }
+
     val baseDir = Paths.get(".")
     val javaHome = "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"
 
     shellScript("git clone git@github.com:facebook/rocksdb.git", baseDir.toFile(), javaHome)
 
     val gitRepoDir = Paths.get("rocksdb")
-    shellScript("git checkout v$VERSION", gitRepoDir.toFile(), javaHome)
+    shellScript("git checkout v$version", gitRepoDir.toFile(), javaHome)
 
     shellScript("brew install cmake", gitRepoDir.toFile(), javaHome)
 
@@ -22,7 +22,7 @@ fun main() {
 
     shellScript("make -j8 rocksdbjava", gitRepoDir.toFile(), javaHome)
 
-    val versionedJarName = "rocksdbjni-$VERSION-osx.jar"
+    val versionedJarName = "rocksdbjni-$version-osx.jar"
     val jar = gitRepoDir.resolve("java").resolve("target").resolve(versionedJarName).toFile()
     val destPath = Paths.get("rocksdbjni-osx.jar").toFile()
     jar.copyTo(destPath)
