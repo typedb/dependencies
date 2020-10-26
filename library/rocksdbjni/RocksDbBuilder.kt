a7c778f4f7b0867a044e211cb9577ad42cdddc40
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-10-26 16:01:25 +0000

    refactor

--- library/rocksdbjni/RocksDbBuilder.kt
+++ library/rocksdbjni/RocksDbBuilder.kt
@@ -2,30 +2,30 @@ package library.rocksdbjni
 
 import org.zeroturnaround.exec.ProcessExecutor
 import org.zeroturnaround.exec.ProcessResult
-import java.io.File
+import java.nio.file.Path
 import java.nio.file.Paths
 
 fun main() {
     val baseDir = Paths.get(".")
     val version = Paths.get("library").resolve("rocksdbjni").resolve("VERSION").toFile().useLines { it.firstOrNull() }
     val javaHome = Paths.get(
-            shellScript("/usr/libexec/java_home", baseDir.toFile(), null)!!.outputUTF8().trim()
-    ).toFile()
+            shellScript("/usr/libexec/java_home", baseDir, null)!!.outputUTF8().trim()
+    )
 
-    shellScript("git clone git@github.com:facebook/rocksdb.git", baseDir.toFile(), javaHome)
+    shellScript("git clone git@github.com:facebook/rocksdb.git", baseDir, javaHome)
 
     val rocksDbDir = Paths.get("rocksdb")
-    shellScript("git checkout v$version", rocksDbDir.toFile(), javaHome)
+    shellScript("git checkout v$version", rocksDbDir, javaHome)
 
-    shellScript("brew install cmake", rocksDbDir.toFile(), javaHome)
+    shellScript("brew install cmake", rocksDbDir, javaHome)
 
-    shellScript("make clean jclean", rocksDbDir.toFile(), javaHome)
+    shellScript("make clean jclean", rocksDbDir, javaHome)
 
-    shellScript("make -j8 rocksdbjava", rocksDbDir.toFile(), javaHome)
+    shellScript("make -j8 rocksdbjava", rocksDbDir, javaHome)
 
     val srcMainJavaDir = Paths.get("rocksdb", "java", "src", "main", "java")
     val sourcesJarName = "rocksdbjni-$version-sources.jar"
-    shellScript("jar -cf ../../../target/$sourcesJarName org", srcMainJavaDir.toFile(), javaHome)
+    shellScript("jar -cf ../../../target/$sourcesJarName org", srcMainJavaDir, javaHome)
 
     val versionedJarName = "rocksdbjni-$version-osx.jar"
     val jar = rocksDbDir.resolve("java").resolve("target").resolve(versionedJarName).toFile()
@@ -37,14 +37,14 @@ fun main() {
     sourcesJar.copyTo(sourcesDestPath)
 }
 
-fun shellScript(cmd: String, baseDir: File, javaHome: File?): ProcessResult? {
-    println(cmd)
-    var builder = ProcessExecutor(cmd.split(" "))
+fun shellScript(script: String, baseDir: Path, javaHome: Path?): ProcessResult? {
+    println(script)
+    var builder = ProcessExecutor(script.split(" "))
             .readOutput(true)
             .redirectOutput(System.out).redirectError(System.err)
-            .directory(baseDir)
+            .directory(baseDir.toFile())
     if (javaHome != null) {
-        builder = builder.environment("JAVA_HOME", javaHome.toPath().toAbsolutePath().toString())
+        builder = builder.environment("JAVA_HOME", javaHome.toAbsolutePath().toString())
     }
     return builder.execute()
 }
