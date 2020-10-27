7f55e0882571e8475a475660447200569483cba6
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-10-27 11:21:43 +0000

    refactor

--- library/rocksdbjni/RocksDbBuilder.kt
+++ library/rocksdbjni/RocksDbBuilder.kt
@@ -4,11 +4,13 @@ import org.zeroturnaround.exec.ProcessExecutor
 import org.zeroturnaround.exec.ProcessResult
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.test.assertNotNull
 
 fun main() {
     val baseDir = Paths.get(".")
     val version = Paths.get("library").resolve("rocksdbjni").resolve("VERSION").toFile().useLines { it.firstOrNull() }
-    val javaHome = Paths.get(shellScript("/usr/libexec/java_home", baseDir, null)!!.outputUTF8().trim())
+
+    val javaHome = Paths.get(shellScript("/usr/libexec/java_home", baseDir, null).outputUTF8().trim())
 
     shellScript("git clone git@github.com:facebook/rocksdb.git", baseDir, javaHome)
 
@@ -35,8 +37,8 @@ fun main() {
     sourcesJar.copyTo(sourcesDestPath)
 }
 
-fun shellScript(script: String, baseDir: Path, javaHome: Path?): ProcessResult? {
-    println(script)
+fun shellScript(script: String, baseDir: Path, javaHome: Path?): ProcessResult {
+    println("RocksDbBuilder.kt: '$script'")
     var builder = ProcessExecutor(script.split(" "))
             .readOutput(true)
             .redirectOutput(System.out)
