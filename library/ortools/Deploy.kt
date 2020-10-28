f1a034d033dc1ce6abf43dbfa4b26c220df2d95f
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-10-28 12:40:15 +0000

    upload all artifacts - wip

--- library/ortools/Deploy.kt
+++ library/ortools/Deploy.kt
@@ -19,16 +19,31 @@ fun main() {
     val GROUP_ID = "com/google/ortools"
     val VERSION = "8.0.8283"
 
-    val ORTOOLS_DARWIN_ARTIFACT_ID = "ortools-darwin-test-8"
+    /*
+     * ortools-darwin
+     */
+    val ORTOOLS_DARWIN_ARTIFACT_ID = "ortools-darwin"
     val ORTOOLS_DARWIN_POM_PATH = Paths.get("external/ortools_osx/pom-runtime.xml")
-//    val ORTOOLS_DARWIN_JAR_PATH = Paths.get("external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION.jar")
-    val ORTOOLS_DARWIN_JAR_PATH = Paths.get("external/ortools_osx/ortools-darwin-8.0.8283.jar")
-//    val ORTOOLS_DARWIN_SRCJAR_PATH = Paths.get("external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION-sources.jar")
-    val ORTOOLS_DARWIN_SRCJAR_PATH = Paths.get("external/ortools_osx/ortools-darwin-8.0.8283-sources.jar")
+    val ORTOOLS_DARWIN_JAR_PATH = Paths.get("external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION.jar")
+    val ORTOOLS_DARWIN_SRCJAR_PATH = Paths.get("external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION-sources.jar")
 
     deployMaven(ORTOOLS_DARWIN_POM_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_DARWIN_ARTIFACT_ID, VERSION, "", "pom")
     deployMaven(ORTOOLS_DARWIN_JAR_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_DARWIN_ARTIFACT_ID, VERSION,  "", "jar")
     deployMaven(ORTOOLS_DARWIN_SRCJAR_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_DARWIN_ARTIFACT_ID, VERSION, "-sources","jar")
+
+    /*
+     * ortools-java-darwin
+     */
+    val ORTOOLS_JAVA_ARTIFACT_ID = "ortools-java-darwin"
+    val ORTOOLS_JAVA_POM_PATH = Paths.get("external/ortools_osx/pom-local.xml")
+    val ORTOOLS_JAVA_JAR_PATH = Paths.get("external/ortools_osx/ortools-java-$VERSION.jar")
+    val ORTOOLS_JAVA_SRCJAR_PATH = Paths.get("external/ortools_osx/ortools-java-$VERSION-sources.jar")
+    val ORTOOLS_JAVA_JAVADOC_PATH = Paths.get("external/ortools_osx/ortools-java-$VERSION-javadoc.jar")
+
+    deployMaven(ORTOOLS_JAVA_POM_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_JAVA_ARTIFACT_ID, VERSION, "", "pom")
+    deployMaven(ORTOOLS_JAVA_JAR_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_JAVA_ARTIFACT_ID, VERSION,  "", "jar")
+    deployMaven(ORTOOLS_JAVA_SRCJAR_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_JAVA_ARTIFACT_ID, VERSION, "-sources","jar")
+    deployMaven(ORTOOLS_JAVA_JAVADOC_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_JAVA_ARTIFACT_ID, VERSION, "-javadoc","jar")
 }
 
 fun deployMaven(source: Path, username: String, password: String, repository: String, groupId: String, artifactId: String, version: String, modifier: String, extension: String) {
