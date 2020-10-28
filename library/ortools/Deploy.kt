9447101b2c514e40041004d53616bcb17cc89e9a
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-10-28 11:52:45 +0000

    use curl in kotlin successfully

--- library/ortools/Deploy.kt
+++ library/ortools/Deploy.kt
@@ -1,11 +1,11 @@
 package library.ortools
 
-import java.io.DataOutputStream
-import java.net.HttpURLConnection
-import java.net.URL
-import java.nio.charset.StandardCharsets.UTF_8
+import org.zeroturnaround.exec.ProcessExecutor
+import org.zeroturnaround.exec.ProcessResult
+import java.lang.RuntimeException
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.*
-import javax.net.ssl.HttpsURLConnection
 
 fun main() {
     val DEPLOY_MAVEN_USERNAME = Objects.requireNonNull(System.getenv("DEPLOY_MAVEN_USERNAME"), "username should be passed via \$DEPLOY_MAVEN_USERNAME env variable")
@@ -15,14 +15,38 @@ fun main() {
     val GROUP_ID = "com/google/ortools"
     val VERSION = "8.0.8283"
 
-//    val ORTOOLS_DARWIN_ARTIFACT_ID = "ortools-darwin"
-//    val ORTOOLS_DARWIN_POM_PATH = "external/ortools_osx/pom-runtime.xml"
-//    val ORTOOLS_DARWIN_JAR_PATH = "external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION.jar"
-//    val ORTOOLS_DARWIN_SRCJAR_PATH = "external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION-sources.jar"
-//
-    post("hello-world".toByteArray(UTF_8), DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, "test", VERSION)
+    val ORTOOLS_DARWIN_ARTIFACT_ID = "ortools-darwin-test-6"
+    val ORTOOLS_DARWIN_POM_PATH = Paths.get("external/ortools_osx/pom-runtime.xml")
+//    val ORTOOLS_DARWIN_JAR_PATH = Paths.get("external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION.jar")
+    val ORTOOLS_DARWIN_JAR_PATH = Paths.get("external/ortools_osx/ortools-darwin-8.0.8283.jar")
+//    val ORTOOLS_DARWIN_SRCJAR_PATH = Paths.get("external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION-sources.jar")
+    val ORTOOLS_DARWIN_SRCJAR_PATH = Paths.get("external/ortools_osx/ortools-darwin-8.0.8283-sources.jar")
+
+    deployMaven(ORTOOLS_DARWIN_POM_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_DARWIN_ARTIFACT_ID, VERSION, "", "pom")
+    deployMaven(ORTOOLS_DARWIN_JAR_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_DARWIN_ARTIFACT_ID, VERSION,  "", "jar")
+    deployMaven(ORTOOLS_DARWIN_SRCJAR_PATH, DEPLOY_MAVEN_USERNAME, DEPLOY_MAVEN_PASSWORD, REPOSITORY_URL, GROUP_ID, ORTOOLS_DARWIN_ARTIFACT_ID, VERSION, "-sources","jar")
 }
 
-fun post(data: ByteArray, username: String, password: String, repository: String, groupId: String, artifactId: String, version: String) {
-//    curl --write-out "%{http_code}" -u "$DEPLOY_MAVEN_USERNAME":"$DEPLOY_MAVEN_PASSWORD" --upload-file "$ORTOOLS_DARWIN_POM_PATH" "$REPOSITORY_URL"/"$GROUP_ID"/"$ORTOOLS_DARWIN_ARTIFACT_ID"/"$VERSION"/"$ORTOOLS_DARWIN_ARTIFACT_ID"-"$VERSION".pom
+fun deployMaven(source: Path, username: String, password: String, repository: String, groupId: String, artifactId: String, version: String, modifier: String, extension: String) {
+    val acceptedExtensions = listOf("pom", "jar", "md5", "sha1", "asc")
+    if (!acceptedExtensions.contains(extension))
+        throw RuntimeException("Unable to upload a file of type $extension. Only $acceptedExtensions are allowed.")
+
+    shellScript(
+            "curl " +
+            "--write-out \"%{http_code}\" " +
+            "-u $username:$password " +
+            "--upload-file $source " +
+            "$repository/$groupId/$artifactId/$version/$artifactId-$version$modifier.$extension"
+    )
+}
+
+fun shellScript(script: String): ProcessResult {
+    val scriptArray = script.split(" ")
+    val builder = ProcessExecutor(scriptArray)
+            .readOutput(true)
+            .redirectOutput(System.out)
+            .redirectError(System.err)
+            .exitValueNormal()
+    return builder.execute()
 }
--- library/ortools/deploy.sh
+++ library/ortools/deploy.sh
@@ -15,6 +15,8 @@ REPOSITORY_URL=https://repo.grakn.ai/repository/maven
 GROUP_ID=com/google/ortools
 VERSION=8.0.8283
 
+curl --write-out '%{http_code}' -u ganesh:smrH91hRXX47xTnBkWez --upload-file external/ortools_osx/pom-runtime.xml https://repo.grakn.ai/repository/maven/com/google/ortools/ortools-darwin-test-4/8.0.8283/ortools-darwin-test-4-8.0.8283.txt
+
 ##################
 # ortools-darwin #
 ##################
@@ -23,7 +25,8 @@ ORTOOLS_DARWIN_POM_PATH=external/ortools_osx/pom-runtime.xml
 ORTOOLS_DARWIN_JAR_PATH=external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION.jar
 ORTOOLS_DARWIN_SRCJAR_PATH=external/ortools_osx/$ORTOOLS_DARWIN_ARTIFACT_ID-$VERSION-sources.jar
 
-curl --write-out "%{http_code}" -u "$DEPLOY_MAVEN_USERNAME":"$DEPLOY_MAVEN_PASSWORD" --upload-file "$ORTOOLS_DARWIN_POM_PATH" "$REPOSITORY_URL"/"$GROUP_ID"/"$ORTOOLS_DARWIN_ARTIFACT_ID"/"$VERSION"/"$ORTOOLS_DARWIN_ARTIFACT_ID"-"$VERSION".pom
+curl --write-out "%{http_code}" -u "$DEPLOY_MAVEN_USERNAME":"$DEPLOY_MAVEN_PASSWORD" --upload-file "$ORTOOLS_DARWIN_POM_PATH"           "$REPOSITORY_URL"/                    "$GROUP_ID"/        "$ORTOOLS_DARWIN_ARTIFACT_ID"/"$VERSION"/"$ORTOOLS_DARWIN_ARTIFACT_ID"-"$VERSION".pom
+curl --write-out "%{http_code}" -u ganesh:smrH91hRXX47xTnBkWez --upload-file external/ortools_osx/pom-runtime.xml https://repo.grakn.ai/repository/maven/com/google/ortools/ortools-darwin-test-3/8.0.8283/ortools-darwin-test-3-8.0.8283.pom
 
 curl --write-out "%{http_code}" -u "$DEPLOY_MAVEN_USERNAME":"$DEPLOY_MAVEN_PASSWORD" --upload-file "$ORTOOLS_DARWIN_POM_PATH" "$REPOSITORY_URL"/"$GROUP_ID"/"$ORTOOLS_DARWIN_ARTIFACT_ID"/"$VERSION"/"$ORTOOLS_DARWIN_ARTIFACT_ID"-"$VERSION".pom.md5
 curl --write-out "%{http_code}" -u "$DEPLOY_MAVEN_USERNAME":"$DEPLOY_MAVEN_PASSWORD" --upload-file "$ORTOOLS_DARWIN_POM_PATH" "$REPOSITORY_URL"/"$GROUP_ID"/"$ORTOOLS_DARWIN_ARTIFACT_ID"/"$VERSION"/"$ORTOOLS_DARWIN_ARTIFACT_ID"-"$VERSION".pom.sha1
