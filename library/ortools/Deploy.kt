84b5ccabd9df07e857b0faf4376061d76d769868
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-10-28 11:23:32 +0000

    use curl hahaha

--- library/ortools/Deploy.kt
+++ library/ortools/Deploy.kt
@@ -5,6 +5,7 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets.UTF_8
 import java.util.*
+import javax.net.ssl.HttpsURLConnection
 
 fun main() {
     val DEPLOY_MAVEN_USERNAME = Objects.requireNonNull(System.getenv("DEPLOY_MAVEN_USERNAME"), "username should be passed via \$DEPLOY_MAVEN_USERNAME env variable")
@@ -23,44 +24,5 @@ fun main() {
 }
 
 fun post(data: ByteArray, username: String, password: String, repository: String, groupId: String, artifactId: String, version: String) {
-    val destination = URL("$repository/$groupId/$artifactId/$version/$artifactId-$version.txt")
-    println("destination: $destination")
-    val connection = destination.openConnection() as HttpURLConnection
-    connection.requestMethod = "POST"
-    connection.connectTimeout = 300000
-    connection.connectTimeout = 300000
-    connection.doOutput = true
-
-//    connection.setRequestProperty("charset", "utf-8")
-    connection.setRequestProperty("Content-length", data.size.toString())
-    connection.setRequestProperty("Content-Type", "application/octet-stream")
-    connection.setRequestProperty(
-            "Authorization",
-            "Basic " + String(Base64.getEncoder().encode("$username:$password".toByteArray()))
-    )
-
-    try {
-        val outputStream = DataOutputStream(connection.outputStream)
-        outputStream.write(data)
-        outputStream.flush()
-    } catch (e: Exception) {
-        throw RuntimeException(e)
-    }
-
-    println("username: $username, password: $password")
-    println("response code: " + connection.responseCode)
-    println("response message: " + connection.responseMessage)
-//    if (connection.responseCode != HttpURLConnection.HTTP_OK && connection.responseCode != HttpURLConnection.HTTP_CREATED) {
-//        try {
-//            val inputStream = DataInputStream(connection.inputStream)
-//            val reader = BufferedReader(InputStreamReader(inputStream))
-//            val output: String = reader.readLine()
-//
-//            println("There was error while connecting the chat $output")
-//            System.exit(0)
-//
-//        } catch (exception: Exception) {
-//            throw Exception("Exception while push the notification  $exception.message")
-//        }
-//    }
+//    curl --write-out "%{http_code}" -u "$DEPLOY_MAVEN_USERNAME":"$DEPLOY_MAVEN_PASSWORD" --upload-file "$ORTOOLS_DARWIN_POM_PATH" "$REPOSITORY_URL"/"$GROUP_ID"/"$ORTOOLS_DARWIN_ARTIFACT_ID"/"$VERSION"/"$ORTOOLS_DARWIN_ARTIFACT_ID"-"$VERSION".pom
 }
