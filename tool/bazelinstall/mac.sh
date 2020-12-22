2ef5557a4fb0d3964324a0d16cf36e33e5446379
Max Vorobev <vmax0770@gmail.com>
2020-12-22 12:39:06 +0300

    Fix tool/bazelinstall/mac.sh (#240)
    ## What is the goal of this PR?
    
    Currently, installing Bazel under Mac in CI (for example, in Core) fails with `Error: Calling brew cask install is disabled! Use brew install [--cask] instead.` message.
    
    ## What are the changes implemented in this PR?
    
    Apply the suggestion and install cask correctly

--- tool/bazelinstall/mac.sh
+++ tool/bazelinstall/mac.sh
@@ -3,7 +3,7 @@
 set -e
 BAZEL_VERSION=3.3.1
 
-brew cask install homebrew/cask-versions/adoptopenjdk8
+brew install --cask homebrew/cask-versions/adoptopenjdk8
 curl -OL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
 chmod +x bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
 sudo ./bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
