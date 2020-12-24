bec7e4103069a08384f32bc1d3bc8e17ff5f5af0
Max Vorobev <vmax0770@gmail.com>
2020-12-24 16:13:38 +0300

    Implement validation of Python dependencies (#243)
    Currently, it's possible to release a Python package which still depends on a snapshot version (say, from `repo.grakn.ai`). In order to prevent it, we need to run a test which goes over `requirements.txt` and verifies that dependencies are properly versioned.
    
    Implement `release_validate_python_deps`; sample usage:
    
    ```
    release_validate_python_deps(
        name = "release-validate-python-deps",
        requirements = "//:requirements.txt",
        tagged_deps = ["graknprotocol"]
    )
    ```

---
+++ tool/release/ValidatePythonDeps.kt
@@ -0,0 +1,50 @@
+package tool.release
+
+import com.eclipsesource.json.Json
+import com.eclipsesource.json.JsonObject
+import com.google.api.client.http.GenericUrl
+import com.google.api.client.http.javanet.NetHttpTransport
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.regex.Pattern
+
+
+fun httpGetJson(url: String?): JsonObject {
+    return Json.parse(NetHttpTransport()
+            .createRequestFactory()
+            .buildGetRequest(GenericUrl(url))
+            .execute()
+            .parseAsString()).asObject()
+}
+
+
+fun main(args: Array<String>) {
+    @Suppress("NAME_SHADOWING") val args = args.toMutableList()
+    val requirements = Files.readAllLines(Paths.get(args.removeAt(0))).filter {
+        !it.startsWith("#") && it.trim().isNotBlank()
+    }.associate {
+        val items = it.replace(">=", "==").split("==")
+        items[0] to items[1]
+    }
+
+    val versionRegex = Pattern.compile("^(\\d+!)?(\\d+)(\\.\\d+)+([\\.\\-\\_])?((a(lpha)?|b(eta)?|c|r(c|ev)?|pre(view)?)\\d*)?(\\.?(post|dev)\\d*)?\$")
+    val pypiAddress = "https://pypi.org/pypi/%s/json"
+
+    args.forEach {
+        val version = requirements.get(it)
+                ?: throw RuntimeException("dependency is not present in requirements.txt: $it")
+
+        if (!versionRegex.matcher(version).matches()) {
+            throw RuntimeException("invalid version of $it: $version")
+        }
+
+        val url = pypiAddress.format(it)
+        try {
+            httpGetJson(url).get("releases").asObject().get(version).asArray()
+        } catch (e: Exception) {
+            throw RuntimeException("cannot download version $version of $it (url $url); please ensure it has been released to pypi.org")
+        }
+    }
+
+}
