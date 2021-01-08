b602038674e26cbdc9c0959e5e4de8938867e9b9
Max Vorobev <vmax0770@gmail.com>
2021-01-08 19:30:50 +0300

    Bump year in license headers to 2021 (#248)
    ## What is the goal of this PR?
    
    License headers are currently out of date (2020) for a while. This PR replaces 2020 with 2021 across all the files.
    
    ## What are the changes implemented in this PR?
    
    * Replace `2020` with `2021` in source files

--- tool/bazelrun/rbe.py
+++ tool/bazelrun/rbe.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Copyright (C) 2020 Grakn Labs
+# Copyright (C) 2021 Grakn Labs
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
