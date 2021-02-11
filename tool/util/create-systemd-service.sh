36bc9911558dc4a81f1f467d5a080033b62852fe
Max Vorobev <vmax0770@gmail.com>
2021-02-11 15:08:01 +0300

    Tweak //tool/util:create-systemd-service to work

--- tool/util/create-systemd-service.sh
+++ tool/util/create-systemd-service.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-service=/tmp/"${1}".service
+service=/tmp/"${2}".service
 
-cp ci/systemd-service-template.txt $service
+cp $1 $service
 
-sed -i -e "s/DESCRIPTION_PLACEHOLDER/${1}/g" $service
-sed -i -e "s/EXEC_START_PLACEHOLDER/${2}/g" $service
+sed -i -e "s/DESCRIPTION_PLACEHOLDER/${2}/g" $service
+sed -i -e "s/EXEC_START_PLACEHOLDER/${3}/g" $service
 
 cat $service
 
