22c0e8a29ca2181c72a7b385fd533ded4b4e8c31
Max Vorobev <vmax0770@gmail.com>
2021-03-09 20:43:50 +0300

    Change the strategy to reply multiple times

--- tool/util/wait-for-host.kt
+++ tool/util/wait-for-host.kt
@@ -5,18 +5,23 @@ import java.net.UnknownHostException
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    var attempt = 0;
-    val host = args[0]
-    while (attempt++ < 100) {
-        println("Waiting for host ${host}: (attempt $attempt).")
-        try {
-            InetAddress.getAllByName(host)
-            println("Host ${host} available after $attempt attempts).")
-            return;
-        } catch (e: UnknownHostException) {
-            Thread.sleep(2000L);
+    for (i in 1..6) {
+        var attempt = 0;
+        val host = args[0]
+        var hostIsUnavailable = true
+        while (hostIsUnavailable && attempt++ < 100) {
+            println("Waiting for host $host: (attempt $i/$attempt).")
+            try {
+                InetAddress.getAllByName(host)
+                println("Host $host available after $i/$attempt attempts).")
+                hostIsUnavailable = false
+            } catch (e: UnknownHostException) {
+                Thread.sleep(2000L);
+            }
+        }
+        if (hostIsUnavailable) {
+            println("ERROR: $attempt attempts reached, but host is still unreachable")
+            exitProcess(1)
         }
     }
-    println("ERROR: $attempt attempts reached, but host is still unreachable")
-    exitProcess(1)
 }
