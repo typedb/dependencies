4a5435aaaf15f2a261eee8af1e1f48051395636f
Max Vorobev <vmax0770@gmail.com>
2021-03-10 15:04:44 +0300

    bash wrapper script

---
+++ tool/util/wait-for-host.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+[[ $(readlink $0) ]] && path=$(readlink $0) || path=$0
+WAIT_FOR_HOST_HOME=$(cd "$(dirname "${path}")" && pwd -P)
+exec java -jar $WAIT_FOR_HOST_HOME/wait-for-host.jar 
+
