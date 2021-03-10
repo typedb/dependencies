de214c70c9ca4b96b0ce96c4ecce6c892956e50c
Max Vorobev <vmax0770@gmail.com>
2021-03-10 15:01:45 +0300

    separate waiting to a new function

--- tool/util/wait-for-host.kt
+++ tool/util/wait-for-host.kt
@@ -4,24 +4,31 @@ import java.net.InetAddress
 import java.net.UnknownHostException
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-    for (i in 1..6) {
-        var attempt = 0;
-        val host = args[0]
-        var hostIsAvailable = false
-        while (!hostIsAvailable && attempt++ < 100) {
-            println("Waiting for host $host: (attempt $i/$attempt).")
-            try {
-                InetAddress.getAllByName(host)
-                println("Host $host available after $i/$attempt attempts).")
-                hostIsAvailable = true
-            } catch (e: UnknownHostException) {
-                Thread.sleep(2000L);
-            }
+fun ensureHostIsAvailable(host: String) {
+    var attempt = 0;
+    var hostIsAvailable = false
+    while (!hostIsAvailable && attempt++ < 100) {
+        println("Waiting for host $host: (attempt $attempt).")
+        try {
+            InetAddress.getAllByName(host)
+            println("Host $host available after $attempt attempts).")
+            hostIsAvailable = true
+        } catch (e: UnknownHostException) {
+            Thread.sleep(2000L);
         }
-        if (!hostIsAvailable) {
-            println("ERROR: $attempt attempts reached, but host is still unreachable")
-            exitProcess(1)
+    }
+    if (!hostIsAvailable) {
+        println("ERROR: $attempt attempts reached, but host is still unreachable")
+        exitProcess(1)
+    }
+}
+
+fun main(hosts: Array<String>) {
+    val totalAttempts = 6
+    for (host in hosts) {
+        for (attempt in 1..totalAttempts) {
+            println("Verifying whether host $host is available: attempt $attempt of $totalAttempts")
+            ensureHostIsAvailable(host)
         }
     }
 }
