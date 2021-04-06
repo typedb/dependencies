54ec8e4958c4cbd07f09273e2bab353abf6e6341
Max Vorobev <vmax0770@gmail.com>
2021-04-06 14:03:20 +0300

    Upgrade Bazel to 4.0.0

--- tool/bazelinstall/mac.sh
+++ tool/bazelinstall/mac.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-BAZEL_VERSION=3.3.1
+BAZEL_VERSION=4.0.0
 
-brew install --cask homebrew/cask-versions/adoptopenjdk8
 curl -OL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
 chmod +x bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
 sudo ./bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
