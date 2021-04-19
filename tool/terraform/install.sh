1126f29ddc6bba4e23210732809cd3430d61a8ff
Max Vorobev <vmax0770@gmail.com>
2021-04-19 18:13:58 +0300

    Upgrade Terraform to 0.15.0

--- tool/terraform/install.sh
+++ tool/terraform/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export TERRAFORM_VERSION="0.12.26"
+export TERRAFORM_VERSION="0.15.0"
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
