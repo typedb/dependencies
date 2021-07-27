de952ce1405bfdb7a24a8e5fb80269ba1e7f75bd
Max Vorobev <vmax0770@gmail.com>
2021-07-27 16:29:27 +0300

    Eliminate duplication by splitting out version logic & Docker org/repo

--- images/docker/ubuntu/version-docker.sh
+++ images/docker/ubuntu/version-docker.sh
@@ -16,11 +16,4 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-set -ex
-
-DOCKER_VERSION_LIB=$2
-DOCKER_VERSION=`$DOCKER_VERSION_LIB`
-DOCKER_ORG=$3
-DOCKER_REPO=$4
-
-docker build -f $1 -t $DOCKER_ORG/$DOCKER_REPO:$DOCKER_VERSION .
+echo "$(git -C $BUILD_WORKSPACE_DIRECTORY rev-parse HEAD)"
