14318a72c20266ce75517b5a58fdc167f06fceda
Max Vorobev <vmax0770@gmail.com>
2021-08-06 14:37:18 +0300

    Upgrade Nexus on repo.grakn.ai and change restart policy

--- tool/util/repo.grakn.ai.sh
+++ tool/util/repo.grakn.ai.sh
@@ -1,1 +1,1 @@
-docker run -d -p 443:8443 -p 80:8081 --name nexus-repo -v nexus-data:/nexus-data -e NEXUS_CONTEXT=/ nexus-repository-apt:3.15.2
+docker run -d -p 443:8443 -p 80:8081 --restart=on-failure --name nexus -v nexus-data:/nexus-data -e NEXUS_CONTEXT=/ sonatype/nexus3:3.33.0
