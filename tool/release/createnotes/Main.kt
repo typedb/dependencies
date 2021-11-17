b5ec55cb39fe30e33786dd02c3aee7bc06bf2eb0
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-11-17 19:43:51 +0000

    Collect release notes from commits (#322)
    ## What is the goal of this PR?
    
    We've updated the release notes script to collect from commits rather than milestones. It will collect the commits between the last release up to the specified commit being released.
    
    Additionally, it's been rewritten in Kotlin.
    
    ## What are the changes implemented in this PR?
    
    1. Update the script for collecting release note from commits, which works as follows:
        - given C1, the commit that we want to release, collect L, the list of commits that we want to add to the release note:
          - if this is the first release ever, L would contain all the commits from C1 down to and including the first commit in the repo
          - if this is not the first release:
            - find C2, the commit of the release that immediately precedes this release
            - L would contain all the commits from C1 down to but excluding C2
        - write PR description or commit message for each commit in L into the release note
    2. Rewrite in Kotlin

---
+++ tool/release/createnotes/Main.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2021 Vaticle
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.vaticle.dependencies.tool.release.createnotes
+
+import java.nio.file.Paths
+import kotlin.io.path.notExists
+
+fun main(args: Array<String>) {
+    val bazelWorkspaceDir = Paths.get(getEnv("BUILD_WORKSPACE_DIRECTORY"))
+    val githubToken = getEnv("CREATE_NOTES_TOKEN")
+    if (args.size != 5) throw RuntimeException("org, repo, version, commit, and template must be supplied")
+    val (org, repo, version, commit, templateFileLocation) = args
+    val templateFile = bazelWorkspaceDir.resolve(templateFileLocation)
+    if (templateFile.notExists()) throw RuntimeException("Template file '$templateFile' does not exist.")
+
+    println("Repository: $org/$repo")
+    println("Commit: $commit")
+    println("Version: $version")
+
+    val commits = getCommits(org, repo, Version.parse(version), commit, bazelWorkspaceDir, githubToken)
+    println("There are ${commits.size} commits to be collected.")
+    val commitDescriptions = getCommitDescriptions(org, repo, commits, githubToken)
+    writeReleaseNoteMd(commitDescriptions, templateFile)
+}
+
+private fun getEnv(env: String): String {
+    return System.getenv(env) ?: throw RuntimeException("'$env' environment variable must be set.")
+}
