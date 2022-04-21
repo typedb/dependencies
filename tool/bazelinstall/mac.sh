d65f1c6763adc64187391494fe73728167684c32
Ganeshwara Hananda <ganesh@vaticle.com>
2022-04-21 10:13:21 +0100

    Remove hardcoded version from the 'Bazel install' script

--- tool/bazelinstall/mac.sh
+++ tool/bazelinstall/mac.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-BAZEL_VERSION=5.1.1
+
+BAZEL_VERSION_FILE=.bazelversion
+if [ ! -e $BAZEL_VERSION_FILE ]
+then
+  echo "Could not find the '$BAZEL_VERSION_FILE' file"
+  exit 1
+fi
+BAZEL_VERSION=$(cat $BAZEL_VERSION_FILE)
 
 curl -OL https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
 chmod +x bazel-${BAZEL_VERSION}-installer-darwin-x86_64.sh
