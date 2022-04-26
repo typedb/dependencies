01ed60adef4667de384917feafda4a50b36f701f
Alex Walker <alex@vaticle.com>
2022-04-26 11:59:16 +0100

    Don't create a milestone

--- tool/release/version/Bump.kt
+++ tool/release/version/Bump.kt
@@ -1,19 +1,7 @@
 package com.vaticle.dependencies.tool.release.version
 
-import com.google.api.client.http.ByteArrayContent
-import com.google.api.client.http.GenericUrl
-import com.google.api.client.http.HttpHeaders
-import com.google.api.client.http.javanet.NetHttpTransport
 import java.nio.file.Files
 import java.nio.file.Paths
-
-fun postJson(url: String?, authorization: String, accept: String, content: String) {
-    NetHttpTransport()
-            .createRequestFactory()
-            .buildPostRequest(GenericUrl(url), ByteArrayContent.fromString("application/json", content))
-            .setHeaders(HttpHeaders().setAuthorization(authorization).setAccept(accept))
-            .execute()
-}
 
 fun bumpVersion(version: String): String {
     val versionComponents = version.split(".").toTypedArray()
@@ -45,24 +33,11 @@ fun bumpVersion(version: String): String {
 fun main() {
     val workspaceDirectory = System.getenv("BUILD_WORKSPACE_DIRECTORY")
             ?: throw RuntimeException("Not running from within Bazel workspace")
-    val githubToken = System.getenv("GITHUB_TOKEN")
-            ?: throw RuntimeException("GITHUB_TOKEN environment variable is not set")
-    val repoOwner = System.getenv("GRABL_OWNER")
-            ?: throw RuntimeException("GRABL_OWNER environment variable is not set")
-    val repoName = System.getenv("GRABL_REPO")
-            ?: throw RuntimeException("GRABL_REPO environment variable is not set")
 
     val versionFile = Paths.get(workspaceDirectory, "VERSION")
     val version = String(Files.readAllBytes(versionFile)).trim()
     val newVersion = bumpVersion(version)
 
-    println("Bumping the version to ${newVersion}")
+    println("Bumping the version to $newVersion")
     Files.write(versionFile, newVersion.toByteArray())
-
-    println("Creating new milestone ${newVersion} in ${repoOwner}/${repoName}")
-    postJson("https://api.github.com/repos/${repoOwner}/${repoName}/milestones",
-            "Token ${githubToken}",
-            "application/vnd.github.v3+json",
-            """{"title": "${newVersion}"}""")
-
 }
