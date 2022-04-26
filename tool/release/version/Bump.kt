4a235b38252b8f18a8f817231df8f38878d76d83
Alex Walker <alex@vaticle.com>
2022-04-26 16:33:25 +0100

    Push bumped version to Git

--- tool/release/version/Bump.kt
+++ tool/release/version/Bump.kt
@@ -1,14 +1,17 @@
 package com.vaticle.dependencies.tool.release.version
 
+import com.vaticle.bazel.distribution.common.Logging.LogLevel.DEBUG
+import com.vaticle.bazel.distribution.common.Logging.Logger
+import com.vaticle.bazel.distribution.common.shell.Shell
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+
+val logger = Logger(DEBUG)
 
 fun bumpVersion(version: String): String {
     val versionComponents = version.split(".").toTypedArray()
-
-    if (versionComponents.size != 3) {
-        throw RuntimeException("Version is supposed to have three components: x.y.z")
-    }
+    if (versionComponents.size != 3) throw RuntimeException("Version is supposed to have three components: x.y.z")
     var lastVersionComponent = versionComponents[versionComponents.lastIndex]
 
     try {
@@ -23,21 +26,41 @@ fun bumpVersion(version: String): String {
                     ).toString()
             lastVersionComponent = versionSubComponents.joinToString("-")
         } catch (b: NumberFormatException) {
-            throw RuntimeException("invalid version: ${version}")
+            throw RuntimeException("invalid version: $version")
         }
     }
     versionComponents[versionComponents.lastIndex] = lastVersionComponent
     return versionComponents.joinToString(".")
 }
 
+fun gitCommitAndPush(workspacePath: Path, newVersion: String) {
+    val shell = Shell(logger)
+    shell.execute(listOf("git", "add", "VERSION"), baseDir = workspacePath)
+    shell.execute(listOf("git", "commit", "-m", "\"Bump version number to $newVersion\""), baseDir = workspacePath)
+    val maxRetries = 3
+    var retryCount = 0
+    while (retryCount < maxRetries) {
+        shell.execute(listOf("git", "pull"), baseDir = workspacePath)
+        try {
+            shell.execute(listOf("git", "push"), baseDir = workspacePath)
+            break
+        } catch (e: Exception) {
+            // cover the unlikely but possible edge case where someone else has already pushed
+            retryCount++
+        }
+    }
+}
+
 fun main() {
     val workspaceDirectory = System.getenv("BUILD_WORKSPACE_DIRECTORY")
             ?: throw RuntimeException("Not running from within Bazel workspace")
-
-    val versionFile = Paths.get(workspaceDirectory, "VERSION")
+    val workspacePath = Paths.get(workspaceDirectory)
+    val versionFile = workspacePath.resolve("VERSION")
     val version = String(Files.readAllBytes(versionFile)).trim()
     val newVersion = bumpVersion(version)
 
-    println("Bumping the version to $newVersion")
+    logger.debug { "Bumping the version to $newVersion" }
     Files.write(versionFile, newVersion.toByteArray())
+
+    gitCommitAndPush(workspacePath, newVersion)
 }
