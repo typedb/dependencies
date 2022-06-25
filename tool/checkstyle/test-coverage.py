95fb2820a207a185076ed00e6f0ffd6286c4ccb6
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2022-06-25 21:32:43 +0300

    Bump copyright year to 2022 (#356)
    ## What is the goal of this PR?
    
    We update the copyright header year to 2022.
    
    ## What are the changes implemented in this PR?
    
    2021 -> 2022 in all the copyright headers and in the checkstyle regexes.

--- tool/checkstyle/test-coverage.py
+++ tool/checkstyle/test-coverage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #
-# Copyright (C) 2021 Vaticle
+# Copyright (C) 2022 Vaticle
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
