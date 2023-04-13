d08da3c8e37e5ba79e2e4802cbcb8aa64978954d
James Williams <james.williams@vaticle.com>
2023-04-13 14:51:57 +0100

    feat: helpful error on failed base64 decode

--- tool/bazelinstall/rbe.sh
+++ tool/bazelinstall/rbe.sh
@@ -6,9 +6,15 @@ if [[ -n "$BAZEL_CACHE_CREDENTIAL" ]] && [[ -n "$BAZEL_CACHE_URL" ]]; then
     echo "Installing remote cache credential..."
     BAZEL_CACHE_CREDENTIAL_LOCATION=./credential.json
     echo "A remote cache credential is found and will be saved to $BAZEL_CACHE_CREDENTIAL_LOCATION. Artifact and test results will be cached remotely."
-    echo "$BAZEL_CACHE_CREDENTIAL" | base64 -d > $BAZEL_CACHE_CREDENTIAL_LOCATION
-    ( echo "build --remote_cache=$BAZEL_CACHE_URL --google_credentials=$BAZEL_CACHE_CREDENTIAL_LOCATION"; echo "test --cache_test_results=no" ) >> ./.bazel-remote-cache.rc
-    echo "The remote cache has been installed!"
+    BAZEL_CACHE_CREDENTIAL_JSON=$(echo "$BAZEL_CACHE_CREDENTIAL" | base64 -d $BAZEL_CACHE_CREDENTIAL_LOCATION)
+    if [[ -n "$BAZEL_CACHE_CREDENTIAL_JSON" ]]; then
+      ( echo "build --remote_cache=$BAZEL_CACHE_URL --google_credentials=$BAZEL_CACHE_CREDENTIAL_LOCATION"; echo "test --cache_test_results=no" ) >> ./.bazel-remote-cache.rc
+      echo "The remote cache has been installed!"
+    else
+      echo "The remote cache was not installed."
+      echo "\$BAZEL_CACHE_CREDENTIAL was non-empty, but failed to be decoded as base64. Confirm your value is valid."
+    fi
+
 else
     echo "The remote cache was not installed."
     echo "The environment variables \$BAZEL_CACHE_CREDENTIAL and \$BAZEL_CACHE_URL must both be set for remote cache installation."
