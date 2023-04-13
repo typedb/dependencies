36801a0cdcb810097df37921f522bbed795c5e87
James Williams <james.williams@vaticle.com>
2023-04-13 14:22:29 +0100

    feat: bazel cache install script refactored, dependencies for factory

--- tool/bazelinstall/rbe.sh
+++ tool/bazelinstall/rbe.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-if [[ -n "$BAZEL_CACHE_URL" ]]; then
+if [[ -n "$BAZEL_CACHE_CREDENTIAL" ]] && [[ -n "$BAZEL_CACHE_URL" ]]; then
     echo "Installing remote cache credential..."
-    BAZEL_CACHE_CREDENTIAL=/opt/credentials/
-    echo "A remote cache credential is found and will be saved to $BAZEL_CACHE_CREDENTIAL. Artifact and test results will be cached remotely."
-    sudo mkdir -p $BAZEL_CACHE_CREDENTIAL && sudo chmod a+rwx $BAZEL_CACHE_CREDENTIAL
-    ( echo "build --remote_cache=$BAZEL_CACHE_URL"; echo "test --remote_cache=$BAZEL_CACHE_URL"; echo "run --remote_cache=$BAZEL_CACHE_URL" ) >> "$BAZEL_CACHE_CREDENTIAL/bazel-remote-cache.rc"
-    echo "The RBE credential has been installed!"
-    echo "Configuring Python..."
-    pyenv global system system
+    BAZEL_CACHE_CREDENTIAL_LOCATION=./.credential.json
+    echo "A remote cache credential is found and will be saved to $BAZEL_CACHE_CREDENTIAL_LOCATION. Artifact and test results will be cached remotely."
+    echo "$BAZEL_CACHE_CREDENTIAL" | base64 -d > $BAZEL_CACHE_CREDENTIAL_LOCATION
+    ( echo "build --remote_cache=$BAZEL_CACHE_URL --google_credentials=$BAZEL_CACHE_CREDENTIAL_LOCATION" ) >> ./.bazel-remote-cache.rc
+    echo "The remote cache has been installed!"
 else
-    echo "No RBE credential found. Bazel will be executed locally without RBE support."
-    echo "Configuring Python..."
-    # uses system version of python, according to the image that is set
-    pyenv global system system
+    echo "The remote cache was not installed."
+    echo "The environment variables \$BAZEL_CACHE_CREDENTIAL and \$BAZEL_CACHE_URL must both be set for remote cache installation."
 fi
