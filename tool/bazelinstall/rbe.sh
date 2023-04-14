0316775703ac3f875f1d04718e95eea60f8a2052
James Williams <james.williams@vaticle.com>
2023-04-14 16:58:08 +0100

    feat: single redirect

--- tool/bazelinstall/rbe.sh
+++ tool/bazelinstall/rbe.sh
@@ -8,7 +8,7 @@ if [[ -n "$BAZEL_CACHE_CREDENTIAL" ]] && [[ -n "$BAZEL_CACHE_URL" ]]; then
     echo "A remote cache credential is found and will be saved to $BAZEL_CACHE_CREDENTIAL_LOCATION. Artifact and test results will be cached remotely."
     BAZEL_CACHE_CREDENTIAL_JSON=$(echo "$BAZEL_CACHE_CREDENTIAL" | base64 -d $BAZEL_CACHE_CREDENTIAL_LOCATION)
     if [[ -n "$BAZEL_CACHE_CREDENTIAL_JSON" ]]; then
-      ( echo "build --remote_cache=$BAZEL_CACHE_URL --google_credentials=$BAZEL_CACHE_CREDENTIAL_LOCATION"; echo "test --cache_test_results=no" ) >> ./.bazel-remote-cache.rc
+      ( echo "build --remote_cache=$BAZEL_CACHE_URL --google_credentials=$BAZEL_CACHE_CREDENTIAL_LOCATION"; echo "test --cache_test_results=no" ) > ./.bazel-remote-cache.rc
       echo "The remote cache has been installed!"
     else
       echo "The remote cache was not installed."
