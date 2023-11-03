2c0d8f6d3de2cc2810ce04d2544555623f594664
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-11-03 11:53:00 +0000

    disable non-camel-case warnings in tonic compile

--- builder/grpc/rust/compile.rs
+++ builder/grpc/rust/compile.rs
@@ -22,6 +22,8 @@ fn main() -> std::io::Result<()> {
     let protos: Vec<&str> = protos_raw.split(";").filter(|&str| !str.is_empty()).collect();
 
     tonic_build::configure()
+        .server_mod_attribute(".", "#[allow(non_camel_case_types)]")
+        .client_mod_attribute(".", "#[allow(non_camel_case_types)]")
         .compile(&protos, &[
             env::var("PROTOS_ROOT").expect("PROTOS_ROOT environment variable is not set")
         ])
