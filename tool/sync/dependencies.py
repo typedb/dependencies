af940f1308030acf6d916587ac197c60e9524cd6
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-01-18 14:23:59 +0000

    make compatible with Factory CI

--- tool/sync/dependencies.py
+++ tool/sync/dependencies.py
@@ -34,25 +34,23 @@ import re
 import subprocess as sp
 import sys
 
-
-IS_CIRCLE_ENV = os.getenv('CIRCLECI')
-if IS_CIRCLE_ENV is None:
-    IS_CIRCLE_ENV = False
+IS_CIRCLECI = bool(os.getenv('CIRCLECI'))
+IS_FACTORY = bool(os.getenv('FACTORY_REPO'))
+IS_CI_ENV = IS_CIRCLECI or IS_FACTORY
 
 GITHUB_TOKEN = os.getenv('SYNC_DEPENDENCIES_TOKEN')
 if GITHUB_TOKEN is None:
     raise Exception("$SYNC_DEPENDENCIES_TOKEN is not set!")
 
 BOT_HOST = 'https://bot.vaticle.com'
-if not IS_CIRCLE_ENV:
+if not IS_CI_ENV:
     BOT_HOST = 'http://localhost:8000'
 
 BOT_SYNC_DEPS = '{0}/sync/dependencies'.format(BOT_HOST)
 
 CMDLINE_PARSER = argparse.ArgumentParser(description='Automatic updater for Vaticle inter-repository dependencies')
-CMDLINE_PARSER.add_argument('--dry-run', help='Do not perform any real actions')  # TODO(vmax): support this argument
+CMDLINE_PARSER.add_argument('--dry-run', help='Do not perform any real actions')
 CMDLINE_PARSER.add_argument('--source', required=True)
-CMDLINE_PARSER.add_argument('--targets', nargs='+', required=True)
 
 COMMIT_SUBJECT_PREFIX = "//ci:sync-dependencies:"
 regex_git_commit = r'[0-9a-f]{40}'
@@ -65,7 +63,12 @@ github_org = github_connection.get_organization(vaticle)
 
 def is_building_upstream():
     """ Returns False is running in a forked repo"""
-    return vaticle in os.getenv('CIRCLE_REPOSITORY_URL', '')
+    if IS_CIRCLECI:
+        return vaticle in os.getenv('CIRCLE_REPOSITORY_URL', '')
+    elif IS_FACTORY:
+        return vaticle == os.getenv('FACTORY_OWNER')
+    else:
+        return False
 
 
 def exception_handler(fun):
@@ -76,9 +79,7 @@ def exception_handler(fun):
         try:
             fun(*args, **kwargs)
         except sp.CalledProcessError as ex:
-            print('An error occurred when running {ex.cmd}. '
-                  'Process exited with code {ex.returncode} '
-                  'and message {ex.output}'.format(ex=ex))
+            print(f'An error occurred when running {ex.cmd}. Process exited with code {ex.returncode} and message {ex.output}')
             print()
             raise ex
 
@@ -86,8 +87,7 @@ def exception_handler(fun):
 
 
 def short_commit(commit_sha):
-    return sp.check_output(['git', 'rev-parse', '--short=7', commit_sha],
-                                   cwd=os.getenv("BUILD_WORKSPACE_DIRECTORY")).decode().replace('\n', '')
+    return sp.check_output(['git', 'rev-parse', '--short=7', commit_sha], cwd=os.getenv("BUILD_WORKSPACE_DIRECTORY")).decode().replace('\n', '')
 
 
 @exception_handler
@@ -97,7 +97,6 @@ def main():
         exit(0)
 
     arguments = CMDLINE_PARSER.parse_args(sys.argv[1:])
-    targets = {}
     source_repo, source_ref = arguments.source.split('@')
 
     if re.match(regex_git_commit, source_ref) is not None:
@@ -106,10 +105,6 @@ def main():
         source_ref_short = source_ref
     else:
         raise ValueError
-
-    for target in arguments.targets:
-        target_repo, target_branch = target.split(':')
-        targets[target_repo] = target_branch
 
     github_repo = github_org.get_repo(source_repo)
     github_commit = github_repo.get_commit(source_ref)
@@ -121,27 +116,26 @@ def main():
     else:
         sync_message = source_message
 
-    print('Requesting the synchronisation of dependency to {0}/{1}@{2} in the following repos:'
-          .format(vaticle, source_repo, source_ref_short))
-    for target_repo in targets:
-        print('- {0}/{1}:{2}'.format(vaticle, target_repo, targets[target_repo]))
+    print('Requesting the synchronisation of dependency to {0}/{1}@{2}'.format(vaticle, source_repo, source_ref_short))
 
     print('Constructing request payload:')
     sync_data = {
         'source-repo': source_repo,
         'source-ref': source_ref,
         'sync-message': sync_message,
-        'targets': targets
     }
     print(str(sync_data))
 
     sync_data_json = json.dumps(sync_data)
     signature = hmac.new(GITHUB_TOKEN.encode(), sync_data_json.encode(), hashlib.sha1).hexdigest()
 
-    print('Sending post request to: ' + BOT_SYNC_DEPS)
-    tc.shell_execute([
-        'curl', '-X', 'POST', '--data', sync_data_json, '-H', 'Content-Type: application/json', '-H', 'X-Hub-Signature: ' + signature, BOT_SYNC_DEPS
-    ])
+    if arguments.dry_run:
+        pass
+    else:
+        print('Sending post request to: ' + BOT_SYNC_DEPS)
+        tc.shell_execute([
+            'curl', '-X', 'POST', '--data', sync_data_json, '-H', 'Content-Type: application/json', '-H', 'X-Hub-Signature: ' + signature, BOT_SYNC_DEPS
+        ])
     print('DONE!')
 
 
