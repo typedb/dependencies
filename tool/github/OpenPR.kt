f24c5259a8b21ad945fef20d7db0dcbc6208cdcf
James Williams <james.williams@vaticle.com>
2024-04-03 17:49:59 +0100

    wip: open PR tool

---
+++ tool/github/OpenPR.kt
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.vaticle.dependencies.tool.github
+
+import picocli.CommandLine
+import java.util.concurrent.Callable
+import kotlin.system.exitProcess
+
+object OpenPR: Callable<Int> {
+    @CommandLine.Option(names = ["--repo"], description = ["The repository to open the PR on"])
+    private lateinit var repo: String
+
+    @CommandLine.Option(names = ["--head-branch"], description = ["The merging branch"])
+    private lateinit var headBranch: String
+
+    @CommandLine.Option(names = ["--base-branch"], description = ["The branch being merged into"])
+    private lateinit var baseBranch: String
+
+    @CommandLine.Option(names = ["--title"], description = ["The title of the PR"])
+    private lateinit var title: String
+
+    @CommandLine.Option(names = ["--token"], description = ["The GitHub authentication token"])
+    private lateinit var token: String
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        exitProcess(CommandLine(OpenPR).execute(*args))
+    }
+
+    override fun call(): Int {
+        return 0
+    }
+}
