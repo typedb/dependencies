da843878ca22ba160a13066d2b3d2a313702fa32
James Williams <james.williams@vaticle.com>
2024-04-04 14:13:48 +0100

    body: defaults for username, body

--- tool/github/OpenPR.kt
+++ tool/github/OpenPR.kt
@@ -12,6 +12,8 @@ import java.util.concurrent.Callable
 import kotlin.system.exitProcess
 
 object OpenPR: Callable<Int> {
+    private const val DEFAULT_USERNAME = "vaticle-bot"
+
     @CommandLine.Option(names = ["--repo"], description = ["The repository to open the PR on"])
     private lateinit var repo: String
 
@@ -25,12 +27,13 @@ object OpenPR: Callable<Int> {
     private lateinit var title: String
 
     @CommandLine.Option(names = ["--body"], description = ["The body of the PR"])
-    private lateinit var body: String
+    private var body: String = ""
+
+    @CommandLine.Option(names = ["--username"], description = ["The GitHub username of the user opening the PR"])
+    private var username: String = DEFAULT_USERNAME
 
     @CommandLine.Option(names = ["--token"], description = ["The GitHub authentication token"])
     private lateinit var token: String
-
-    private const val USERNAME = "vaticle-bot"
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -38,7 +41,7 @@ object OpenPR: Callable<Int> {
     }
 
     override fun call(): Int {
-        val gh = GitHub.connect(USERNAME, token)
+        val gh = GitHub.connect(username, token)
         gh.getRepository(repo).createPullRequest(title, headBranch, baseBranch, body)
         return 0
     }
