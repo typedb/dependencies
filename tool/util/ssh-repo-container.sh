9a440b77ac766bf80241ecab81c454d3745f3516
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-13 10:02:46 +0000

    Delete old repo.grakn.ai scripts

--- tool/util/ssh-repo-container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-# TODO: transition over to repo-vaticle-com
-exec gcloud compute ssh repo-grakn-ai --project grakn-dev --zone europe-west1-b -- docker exec -it nexus bash
