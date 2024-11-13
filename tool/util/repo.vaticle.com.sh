367a198bc4410f0c520a24015e8af83d768ba533
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-13 16:59:46 +0000

    Delete an old repo.vaticle.com script

--- tool/util/repo.vaticle.com.sh
+++
@@ -1,6 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-
-docker run -d -p 443:8443 -p 80:8081 --restart=on-failure --name nexus -v nexus-data:/nexus-data -e NEXUS_CONTEXT=/ sonatype/nexus3:3.33.0
